a71980b7cd05023fc511fc4336f4b3b1fd8497c1
jsaj <66035752+jsaj@users.noreply.github.com>
2022-02-04 17:03:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,40 @@
-#!/usr/bin/env python
+import setuptools
 
-import codecs
-import os
-from distutils.core import setup
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-from setuptools import find_packages
-
-setup_path = os.path.abspath(os.path.dirname(__file__))
-with codecs.open(os.path.join(setup_path, 'README.rst'), encoding='utf-8-sig') as f:
-    README = f.read()
-
-setup(name='DSSC',
-      packages=['dssc'],
-      version='0.0.1',
-      url='https://github.com/jsaj/DSSC',
-      maintainer='Juscelino S. A. Júnior',
-      maintainer_email='j.jr.avelino@gmail.com',
-      description='Implementation of approach to Cross-Project Defect Prediction with Dynamic Ensemble Selection methods',
-      long_description=README,
-      author='Juscelino S. A. Júnior',
-      author_email='j.jr.avelino@gmail.com',
-      license='MIT',
-
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence',
-      ],
-      install_requires=[
+setuptools.setup(
+    name='dssc',  # should match the package folder
+    packages=['dssc'],  # should match the package folder
+    version='0.0.1',  # important for updates
+    license='MIT',  # should match your chosen license
+    maintainer='Juscelino S. A. Júnior',
+    maintainer_email='j.jr.avelino@gmail.com',
+    description='Implementation of approach to Cross-Project Defect Prediction with Dynamic Ensemble Selection methods',
+    long_description=long_description,  # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author='Juscelino S. A. Júnior',
+    author_email='j.jr.avelino@gmail.com',
+    url='https://github.com/jsaj/DSSC',
+    install_requires=[
           'scikit-learn>=0.21.0',
           'numpy>=1.17.0',
           'scipy>=1.4.0',
-      ],
-      python_requires='>=3',      
+    ],
+    
+    keywords=["cpdp", "dynamic selection", "prediction", "classification", "defect prediction"],  # descriptive meta-data
+    classifiers=[  # https://pypi.org/classifiers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 
-      packages=find_packages())
+#     download_url="https://github.com/mike-huls/py-console/archive/refs/tags/0.1.4.tar.gz",
+)
